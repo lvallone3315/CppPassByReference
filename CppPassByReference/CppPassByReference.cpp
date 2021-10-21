66356314022ced6d8472f7fdf5993f69fba1f152
lvallone3315 <User@DESKTOP-0OFPPJL>
2021-10-21 13:41:27 -0400

    Added various calling options, cleaned up output, added pauses in between options

--- CppPassByReference/CppPassByReference.cpp
+++ CppPassByReference/CppPassByReference.cpp
@@ -3,36 +3,102 @@
 
 #include <iostream>
 
+class TwoInts {
+public:
+    int a;
+    int b;
+}; 
+
 void swap(int, int);
+void swapC(TwoInts);
 void swapByReference(int&, int&);
+void swapCByReference(TwoInts&);
+TwoInts swapByNewInstance(TwoInts);
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "\t\tSwap Program Demonstrating C++ Pass by Value & Pass by Reference!\n\n\n";
 
-    int a2 = 2;
-    int b3 = 3;
+    int a2 = 1;
+    int b3 = 99;
 
+    TwoInts ab;
 
     swap(a2, b3);
-    std::cout << "Main: a = " << a2 << "\tb = " << b3 << "\n\n\n";
+    std::cout << "\tActual: \ta = " << a2 << "\tb = " << b3 << "\n\n";
+    system("pause");
 
-    // swapByReference(a2, b3);
-    // std::cout << "Main: a = " << a2 << "\tb = " << b3 << "\n";
+    a2 = 1;
+    b3 = 99;
+
+    swapByReference(a2, b3);
+    std::cout << "\tMain: \t\t\ta = " << a2 << "\tb = " << b3 << "\n";
+    system("pause");
+
+    ab.a = 1;
+    ab.b = 99;
+    swapC(ab);
+    std::cout << "\tMain TwoInts: \t\ta = " << ab.a << "\tb = " << ab.b << "\n\n";
+    system("pause");
+
+
+    ab.a = 1;
+    ab.b = 99; 
+    swapCByReference(ab);
+    std::cout << "\tMain TwoInts Ref: \ta = " << ab.a << "\tb = " << ab.b << "\n\n";
+    system("pause");
+
+    ab.a = 1;
+    ab.b = 99; 
+    ab = swapByNewInstance(ab);
+    std::cout << "\tMain TwoInts Instance: \ta = " << ab.a << "\tb = " << ab.b << "\n\n";
+    system("pause");
 }
 
 void swap(int a, int b) {
     int temp;
+    std::cout << "*** Two Variables passed in: \t\ta = " << a << "\tb = " << b << "\n";
     temp = a;
     a = b;
     b = temp;
-    std::cout << "Swap: a = " << a << "\tb = " << b << "\n";
+    std::cout << "  *** Expected Result ***: \t\ta = " << a << "\tb = " << b << "\n";
+}
+
+void swapC(TwoInts numbersToSwap) {
+    int temp;
+    std::cout << "\n*** TwoInts instance passed in - by Value*\n";
+    temp = numbersToSwap.a;
+    numbersToSwap.a = numbersToSwap.b;
+    numbersToSwap.b = temp;
 }
 
 void swapByReference(int& a, int& b) {
     int temp;
+
+    std::cout << "\n*** Two variables passed in - by Reference*\n"; 
     temp = a;
     a = b;
     b = temp;
-    std::cout << "Swap: a = " << a << "\tb = " << b << "\n";
+}
+
+void swapCByReference(TwoInts& numbersToSwap) {
+    int temp;
+    std::cout << "\n*** TwoInts instance passed in - by Reference*\n";
+    temp = numbersToSwap.a;
+    numbersToSwap.a = numbersToSwap.b;
+    numbersToSwap.b = temp;
+}
+
+
+// Create a new instance of TwoInts and return to calling routine
+// Note: Pretty sure this is NO GOOD - since new instance probably created on the stack
+TwoInts swapByNewInstance(TwoInts numbersToSwap) {
+    TwoInts tempTwoInts;
+
+    std::cout << "\n*** Create a new instance of TwoInts and return to calling routine\n";
+    tempTwoInts.b = numbersToSwap.a;
+    tempTwoInts.a = numbersToSwap.b;
+    return tempTwoInts;
 }
