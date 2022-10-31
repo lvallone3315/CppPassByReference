d31e8fa3bac10fc35980cbd4637c738c306986e1
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2022-10-31 09:00:29 -0400

    Additional text output to screen to clarify what the program is demonstrating.  Aligning output.

--- CppPassByReference/CppPassByReference.cpp
+++ CppPassByReference/CppPassByReference.cpp
@@ -19,7 +19,12 @@ TwoInts swapByNewInstance(TwoInts);
 
 int main()
 {
-    std::cout << "\t\tSwap Program Demonstrating C++ Pass by Value & Pass by Reference!\n\n\n";
+    std::cout << "\t\tSwap Program Demonstrating C++ Pass by Value & Pass by Reference!\n\n";
+
+    std::cout << "\n\t First two scenarios: pass integer vars by value, then by reference\n";
+    std::cout << "\t Second two scenarios: pass an object of type TwoInts by value, then by reference\n\n\n";
+    std::cout << "\tC++ swap by reference, in called function/method add & between type and param name\n";
+    std::cout << "\t\tfor example: swap(int a, int b) vs. swap (int& a, int& a)\n\n";
 
     int a2 = 1;
     int b3 = 99;
@@ -27,48 +32,48 @@ int main()
     TwoInts ab;
 
     swap(a2, b3);
-    std::cout << "\tActual: \ta = " << a2 << "\tb = " << b3 << "\n\n";
+    std::cout << "\Main swap by values result: \t\t\ta = " << a2 << "\tb = " << b3 << "\n\n";
     system("pause");
 
     a2 = 1;
     b3 = 99;
 
     swapByReference(a2, b3);
-    std::cout << "\tMain: \t\t\ta = " << a2 << "\tb = " << b3 << "\n";
+    std::cout << "\tMain swap result: \t\t\ta = " << a2 << "\tb = " << b3 << "\n";
     system("pause");
 
     ab.a = 1;
     ab.b = 99;
     swapC(ab);
-    std::cout << "\tMain TwoInts: \t\ta = " << ab.a << "\tb = " << ab.b << "\n\n";
+    std::cout << "\tMain TwoInts swap result: \t\ta = " << ab.a << "\tb = " << ab.b << "\n\n";
     system("pause");
 
 
     ab.a = 1;
     ab.b = 99; 
     swapCByReference(ab);
-    std::cout << "\tMain TwoInts Ref: \ta = " << ab.a << "\tb = " << ab.b << "\n\n";
+    std::cout << "\tMain TwoInts Ref swap result: \t\ta = " << ab.a << "\tb = " << ab.b << "\n\n";
     system("pause");
 
     ab.a = 1;
     ab.b = 99; 
     ab = swapByNewInstance(ab);
-    std::cout << "\tMain TwoInts Instance: \ta = " << ab.a << "\tb = " << ab.b << "\n\n";
+    std::cout << "\tMain TwoInts Instance swap result: \ta = " << ab.a << "\tb = " << ab.b << "\n\n";
     system("pause");
 }
 
 void swap(int a, int b) {
     int temp;
-    std::cout << "*** Two Variables passed in: \t\ta = " << a << "\tb = " << b << "\n";
+    std::cout << "*** Swap two variables: \t\t\ta = " << a << "\tb = " << b << "\n";
     temp = a;
     a = b;
     b = temp;
-    std::cout << "  *** Expected Result ***: \t\ta = " << a << "\tb = " << b << "\n";
+    std::cout << "  *** Expected Result ***: \t\t\ta = " << a << "\tb = " << b << "\n\n";
 }
 
 void swapC(TwoInts numbersToSwap) {
     int temp;
-    std::cout << "\n*** TwoInts instance passed in - by Value*\n";
+    std::cout << "\n*** Swap twoInts object - by Value*\n";
     temp = numbersToSwap.a;
     numbersToSwap.a = numbersToSwap.b;
     numbersToSwap.b = temp;
@@ -77,7 +82,7 @@ void swapC(TwoInts numbersToSwap) {
 void swapByReference(int& a, int& b) {
     int temp;
 
-    std::cout << "\n*** Two variables passed in - by Reference*\n"; 
+    std::cout << "\n*** Swap two variables - by Reference*\n"; 
     temp = a;
     a = b;
     b = temp;
@@ -85,7 +90,7 @@ void swapByReference(int& a, int& b) {
 
 void swapCByReference(TwoInts& numbersToSwap) {
     int temp;
-    std::cout << "\n*** TwoInts instance passed in - by Reference*\n";
+    std::cout << "\n*** Swap twoInts object - by Reference*\n";
     temp = numbersToSwap.a;
     numbersToSwap.a = numbersToSwap.b;
     numbersToSwap.b = temp;
